a86bdf9d64b0d592481ff096b4fe4e556d1eeff2
anika-jane <112495457+anika-jane@users.noreply.github.com>
2023-04-12 09:47:34 -0400

    Added the Github Repo URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+     <footer>
+      This porject was coded by Anika Jane and it is Open Sourced on  
+     <a  href="https://github.com/anika-jane/weather-app-react-final" tragert="_blank"> Github</a>
+     </footer>
+     
     </div>
   );
 }
