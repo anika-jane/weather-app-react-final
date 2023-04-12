2fbe331d3262b9ad0403545b83822376097d304e
anika-jane <112495457+anika-jane@users.noreply.github.com>
2023-04-12 10:29:13 -0400

    Added in the HTML portion

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="container">
+        <Weather />
      <footer>
-      This porject was coded by Anika Jane and it is Open Sourced on  
-     <a  href="https://github.com/anika-jane/weather-app-react-final" tragert="_blank"> Github</a>
+      This porject was coded by{" "} Anika Jane and it is{" "}   
+     <a  href="https://github.com/anika-jane/weather-app-react-final" tragert="_blank">open-sourced on Github</a>
      </footer>
-     
+     </div>
     </div>
   );
 }
